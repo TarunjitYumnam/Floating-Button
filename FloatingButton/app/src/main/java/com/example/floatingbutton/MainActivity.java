e6776a12d667df7ce913f36fc085ce00135a3921
Traditional-Attire-Online-Shopping-App <apuningthouja034@gmail.com>
2021-02-09 21:18:38 +0530

    email

---
+++ FloatingButton/app/src/main/java/com/example/floatingbutton/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.floatingbutton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+    FloatingActionButton floatingButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        floatingButton = findViewById(R.id.floating);
+        floatingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMail();
+            }
+        });
+    }
+
+    private void sendMail() {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("Msg/rfc822");
+        startActivity(Intent.createChooser(intent, "Choose One"));
+    }
+
+}
